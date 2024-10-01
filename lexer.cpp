1e897d12f5cde04a6cf880ece6330826ee5444c6
peter chau <Peterchauu93@csu.fullerton.edu>
2024-10-01 11:21:39 -0700

    Initialized our first commit

---
+++ lexer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+//incorporate our finite state machine which returns the token and the lexeme associated with it.
+int lexer(std::string token) {
+    return 0;
+}
+
+
+int main(int argc, char* argv[]) {
+    //Define our main execution here to accept file arguments.
+    return 0;
+}
---
+++ lexer.h
@@ -0,0 +1,6 @@
+/*
+    If we want to split the function and declarations up into 2 separate files
+    we want to implement a class for the entire project and have the 
+    lexer() function as a class function instead, we use this file to 
+    denote any custom functions we make that we want lexer to use.
+*/
